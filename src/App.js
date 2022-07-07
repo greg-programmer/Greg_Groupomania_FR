2faf02a9fb3e26f6d96449d16cd50f4d7c454cdf
greg-programmer <91738933+greg-programmer@users.noreply.github.com>
2022-07-07 03:06:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+//Récupère automatiquement le fichier index.js du dossier routes//
+import Routes from "./components/routes"; 
+import { UidContext } from './components/AppContext';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { getUser } from './actions/user.action';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+   const [uid,setUid] = useState(null);
+  const dispatch = useDispatch();
 
-export default App;
+  //Un controle du token est tout de suite et si c'est pas le bon alors, il est renvoyé//
+   useEffect (() => {
+   const fetchToken = async()=> {
+     await axios ({
+       method:"get",
+       url:"http://localhost:4080/api/auth/login/token",
+       withCredentials:true
+     }) 
+     .then((res)=>{
+       setUid(res.data)})    
+     .catch((err)=>console.log("no token =>",err,window.location()));       
+   };
+   fetchToken();
+   
+   if(uid)dispatch(getUser(uid))
+  },[uid +1]);      
+
+    return(    
+      //{uid} en props pour pouvoir l'utiliser dans toute l'application//
+      <UidContext.Provider value= {uid}>
+ <Routes/>   
+      </UidContext.Provider>    
+    );
+}; 
+
+export default App
